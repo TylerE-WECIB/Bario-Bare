d18da1ca8fc5bb7676ffe5617b4cf2635d15c30a
Tyler E <145382438+TylerE-WECIB@users.noreply.github.com>
2025-04-28 09:29:53 -0400

    added sfx and improved bomb

--- bariobare/Scripts/game_ui.gd
+++ bariobare/Scripts/game_ui.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
 func _ready() -> void:
-	pass
+	$AnimationPlayer.play("fuse_test")
 
 func _physics_process(delta: float) -> void:
-	pass
+	$Bomb/Spark.global_position = $Bomb/Path2D/PathFollow2D.global_position
+	$Bomb/Path2D/PathFollow2D.progress_ratio = $Bomb/Fuse.value / $Bomb/Fuse.max_value
+	$Bomb/Spark.rotation += 1
+	
+	if $Bomb/Fuse.value == 0:
+		$Bomb.frame = 1
+		$Bomb/Label.visible = false
+	else:
+		$Bomb.frame = 0
+		$Bomb/Label.visible = true
