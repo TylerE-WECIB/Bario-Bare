2ffc202b52b61722cf389401c360ca31a7870202
pfthomas-1 <pfthomas@my.waketech.edu>
2025-04-28 09:37:00 -0400

    updated default_game.tscn an default_game.gd
    tscn:
    - connected winGame and loseGame signals
    
    gd:
    - setTimeLimit() now returns a value instead of modifying a variable
    - added setup() function which gets called by _ready()

--- bariobare/Scripts/default_game.gd
+++ bariobare/Scripts/default_game.gd
@@ -1,25 +1,52 @@
 extends Node2D
 
 @onready var gameTimer = $GameTimer;
-var timeLimit; # passed into gameTimer.set_wait_time()
 var defaultTimeLimit = 30; # level dependent
 var minTimeLimit = 10; # level dependent
 var timeLimitStep = 3; # how much time limit is decreased by based on difficulty, level dependent
+var winConditionMet = false;
 
 signal winGame
 signal loseGame
 
 func _ready() -> void:
-	pass
+	setup()
 
 func _process(delta: float) -> void:
 	pass
 
+func setup() -> void: # responsible for setting timeLimit and starting the timer
+	print("======== setup() ========");
+	
+	print("1. set level-dependent variables (do later)"); # set level specific variables here
+	defaultTimeLimit = 30;
+	minTimeLimit = 10;
+	timeLimitStep = 3;
+	
+	print("defaultTimeLimit: ", defaultTimeLimit);
+	print("minTimeLimit: ", minTimeLimit);
+	print("timeLimitStep: ", timeLimitStep);
+	print();
+	
+	print("2. start GameTimer using setTimeLimit() as argument");
+	gameTimer.start(setTimeLimit());
+	
+
 
 func _on_game_timer_timeout() -> void:
+	print("======== _on_game_timer_timeout() ========");
 	print("timeout ", gameTimer.get_wait_time());
+	print("winConditionMet: ", winConditionMet)
+	
+	print();
+	
+	if winConditionMet:
+		winGame.emit()
+	else:
+		loseGame.emit()
 
-func setTimeLimit(): # sets the time limit based on difficulty
+func setTimeLimit() -> int: # sets the time limit based on difficulty
+	var levelTimeLimit;
 	print("======== setTimeLimit() ========");
 	
 	var difficulty = Global.difficulty;
@@ -30,11 +57,21 @@ func setTimeLimit(): # sets the time limit based on difficulty
 	print("timeLimitStep: ", timeLimitStep);
 	print("timeLimitModifier: ", timeLimitModifier);
 	
-	timeLimit = defaultTimeLimit - timeLimitModifier;
-	print("timeLimit: ", timeLimit)
+	levelTimeLimit = defaultTimeLimit - timeLimitModifier;
+	print("levelTimeLimit: ", levelTimeLimit)
 	
-	if timeLimit < minTimeLimit:
-		print("timeLimit to small, setting to minTimeLimit: ", minTimeLimit)
-		timeLimit = timeLimit;
+	if levelTimeLimit < minTimeLimit:
+		print("levelTimeLimit is to small, setting to minTimeLimit: ", minTimeLimit)
+		levelTimeLimit = minTimeLimit;
 	
 	print()
+	return levelTimeLimit;
+
+
+func _on_lose_game() -> void:
+	print("======== _on_lose_game() ========");
+	print("You lost")
+
+func _on_win_game() -> void:
+	print("======== _on_win_game() ========");
+	print("You won")
