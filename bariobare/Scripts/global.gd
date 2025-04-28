425af6250fb01d773daf65b65a8403cdcf85a38e
Tyler E <145382438+TylerE-WECIB@users.noreply.github.com>
2025-04-28 08:32:05 -0400

    worked on game ui

--- bariobare/Scripts/global.gd
+++ bariobare/Scripts/global.gd
@@ -1,1 +1,13 @@
 extends Node
+
+#list of microgames. will read from microgame folder
+var microgame_list := []
+
+func _ready() -> void:
+	var dir = DirAccess.open("res://Scenes/Microgames/")
+	for files in dir.get_files():
+		if files != "default_game.tscn":
+			microgame_list.append("res://Scenes/Microgames/"+files)
+	
+	#debug line to read all of the microgames in the folder
+	#print(microgame_list)
