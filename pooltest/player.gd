28dd0592030c05645d4558fc4b7250cc9b8547bb
Nuno <61714135+NunoR1@users.noreply.github.com>
2025-04-29 10:04:43 -0400

    pool

--- pooltest/player.gd
+++ pooltest/player.gd
@@ -1,24 +1,28 @@
-extends Area2D
+extends Sprite2D
+
+signal shoot
 
 var screen_size
 var mouse_cur_pos
-@export var speed = 400
 @export var angle = 90
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
 
-func _process(delta):
-	mouse_cur_pos = get_viewport().get_mouse_position()
-	angle = rad_to_deg(get_angle_to(Vector2(-mouse_cur_pos[0], -mouse_cur_pos[1])))
+func _physics_process(delta: float):
+
+	if !Input.is_action_pressed("grab"):
+		mouse_cur_pos = get_global_mouse_position()
+		look_at(mouse_cur_pos)
 	
-	$cueTip.rotation_degrees = angle
-	
-	
-	#if Input.is_action_pressed("grab") and get_viewport().get_mouse_position() != mouse_cur_pos:
-		
-	
+	if Input.is_action_pressed("grab"):
+		determine_power(mouse_cur_pos)
 	
 	
 	
+func determine_power(mouse):
+	var mouse_up_pos = get_global_mouse_position()
+	var power = 0
+	#if Input.is_action_pressed("grab") and mouse_up_pos != mouse:
+		#PathFollow2D.p
