01be65d733a639f99329b1699195509f3d25614f
Tytaneum <145382438+TylerE-WECIB@users.noreply.github.com>
2025-05-04 12:27:20 -0400

    added difficulty scaling and game over sound

--- bariobare/Scripts/global.gd
+++ bariobare/Scripts/global.gd
@@ -24,7 +24,7 @@ func _ready() -> void:
 	var dir = DirAccess.open("res://Scenes/Microgames/")
 	for files in dir.get_files():
 		if files != "default_game.tscn":
-			microgame_list.append("res://Scenes/Microgames/"+files)
+			microgame_list.append("res://Scenes/Microgames/"+files.trim_suffix(".remap"))
 	#debug line to read all of the microgames in the folder
 	print(microgame_list)
 
